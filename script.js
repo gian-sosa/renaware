3c37cfc9c4bd5f769ddc4d3e9df463d925dbff91
gian-sosa <giancarlos.msosa@gmail.com>
2025-03-14 19:12:20 -0500

    new file:   img/ef1982a3-1f13-408e-b3b1-888f18cf308d.jpeg 	new file:   img/icons8-eliminar.svg 	modified:   index.html 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ menuIcon.onclick = function () {
   nav = document.querySelector(".nav");
   nav.classList.toggle("active");
 };
+
+cerrarImg = document.querySelector(".cerrarImg");
+cerrarImg.onclick = function () {
+  img = document.querySelector(".aviso");
+  img.classList.toggle("closeAviso");
+};
